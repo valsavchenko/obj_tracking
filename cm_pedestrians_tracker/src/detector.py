2fd23d5403650f7fcd1717fc87431a0b3f0d7391
Valentin Savchenko <valentine.savchenko@gmail.com>
2025-06-15 20:48:18 +0300

    Drop a dirty integration with TRT to detect objects

--- cm_pedestrians_tracker/src/detector.py
+++ cm_pedestrians_tracker/src/detector.py
@@ -1,3 +1,4 @@
+import cv2
 import numpy as np
 import pycuda.autoinit  # noqa
 import pycuda.driver as cuda
@@ -36,8 +37,6 @@ class Detector(_ped_trk.timer.Timeable):
         """
         super().__init__(logger=logger)
 
-        cuda.init()
-
         log = _Logger(logger=logger)
         trt.init_libnvinfer_plugins(logger=log, namespace='')
 
@@ -50,40 +49,90 @@ class Detector(_ped_trk.timer.Timeable):
 
         self.__context = self.__engine.create_execution_context()
 
-        self.__inputs, self.__outputs, self.__bindings = [], [], []
-
+        inputs, outputs = [], []
         for binding in self.__engine:
-            size = trt.volume(self.__engine.get_tensor_shape(name=binding))
+            shape = trt.volume(self.__engine.get_tensor_shape(name=binding))
             dtype = trt.nptype(self.__engine.get_tensor_dtype(name=binding))
+            host_memory = cuda.pagelocked_empty(shape=shape, dtype=dtype)
+            device_memory = cuda.mem_alloc(host_memory.nbytes)
 
-            host_mem = cuda.pagelocked_empty(size, dtype)
-            device_mem = cuda.mem_alloc(host_mem.nbytes)
+            puts = inputs if trt.TensorIOMode.INPUT == self.__engine.get_tensor_mode(binding) else outputs
+            puts.append({'name': binding, 'host': host_memory, 'device': device_memory, 'shape': shape})
 
-            self.__bindings.append(int(device_mem))
+        if 1 != len(inputs):
+            raise ValueError(f'Fail to parse CUDA engine with {len(inputs)} inputs and {len(outputs)} outputs')
 
-            puts = self.__inputs if trt.TensorIOMode.INPUT == self.__engine.get_tensor_mode(binding) else self.__outputs
-            puts.append({'host': host_mem, 'device': device_mem})
-
+        self.__input, *_ = inputs
+        self.__outputs = outputs
         self.__stream = cuda.Stream()
+
+    def _pre_process(self, frame):
+        """
+        """
+        # Resize the frame, while maintaining aspect ratio, to match the input tensor
+        tensor_name = self.__input['name']
+        tensor_height, tensor_width = self.__engine.get_tensor_shape(tensor_name)[2:]
+        frame_height, frame_width = frame.shape[:2]
+        scale = min(tensor_width / frame_width, tensor_height / frame_height)
+        resized_width, resized_height = int(scale * frame_width), int(scale * frame_height)
+        resized = cv2.resize(src=frame, dsize=(resized_width, resized_height), interpolation=cv2.INTER_LINEAR)
+
+        # Pad the input tensor
+        pad_top = (tensor_height - resized_height) // 2
+        pad_bottom = tensor_height - resized_height - pad_top
+        pad_left = (tensor_width - resized_width) // 2
+        pad_right = tensor_width - resized_width - pad_left
+        padded = cv2.copyMakeBorder(src=resized, top=pad_top, bottom=pad_bottom, left=pad_left, right=pad_right,
+                                    borderType=cv2.BORDER_CONSTANT, value=(114, 114, 114))
+
+        rgbed = cv2.cvtColor(src=padded, code=cv2.COLOR_BGR2RGB)
+
+        normalized = rgbed.astype(dtype=np.float32) / 255.0
+
+        transposed = np.transpose(normalized, axes=(2, 0, 1))
+
+        batched = np.expand_dims(transposed, axis=0)
+
+        tensor = np.ascontiguousarray(batched, dtype=np.float32)
+
+        return tensor, scale, (pad_top, pad_left)
+
+    def _post_process(self, scale, pad):
+        """
+        """
+        class_objs = []
+
+        pad_top, pad_left = pad
+        for d in range(self.__outputs[0]['host'][0]):
+            left, top, right, bottom = self.__outputs[1]['host'][4 * d:4 * (d + 1)]
+            left = int((left - pad_left) / scale)
+            top = int((top - pad_top) / scale)
+            right = int((right - pad_left) / scale)
+            bottom = int((bottom - pad_top) / scale)
+            lt_wh = left, top, right - left, bottom - top
+
+            score = self.__outputs[2]['host'][d]
+
+            class_objs.append({'lt_wh': lt_wh, 'score': score})
+
+        return class_objs
+
 
     @_ped_trk.timer.timer(label='obj_det')
     def detect(self, frame):
         """
         """
-        height, width, *_ = frame.shape
+        input_tensor, scale, pad = self._pre_process(frame=frame)
 
-        boxes = []
-        num = np.random.randint(low=0, high=5)
-        ls = np.random.randint(low=0, high=width, size=num)
-        ts = np.random.randint(low=0, high=height, size=num)
-        for l, t in zip(ls, ts):
-            if (width - l) < 2 or (height - t) < 2:
-                continue
+        np.copyto(dst=self.__input['host'], src=input_tensor.ravel())
+        cuda.memcpy_htod_async(dest=self.__input['device'], src=self.__input['host'], stream=self.__stream)
+        for put in [self.__input] + self.__outputs:
+            self.__context.set_tensor_address(put['name'], int(put['device']))
+        self.__context.execute_async_v3(stream_handle=self.__stream.handle)
 
-            w = np.random.randint(low=1, high=(width - l))
-            h = np.random.randint(low=1, high=(height - t))
-            boxes.append((l, t, w, h))
-        scores = np.random.rand(len(boxes))
+        for output in self.__outputs:
+            cuda.memcpy_dtoh_async(dest=output['host'], src=output['device'], stream=self.__stream)
+        self.__stream.synchronize()
 
-        class_objs = [{'lt_wh': b, 'score': s} for b, s in zip(boxes, scores)]
+        class_objs = self._post_process(scale=scale, pad=pad)
         return class_objs
